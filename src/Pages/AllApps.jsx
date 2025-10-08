543563809ba480a185f4a27f2bc073e59d5c6dbd
tamanna-hash <tamannaemails@gmail.com>
2025-10-08 22:02:51 +0600

    without strictmode

--- src/Pages/AllApps.jsx
+++ src/Pages/AllApps.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 const AllApps = () => {
     return (
         <div>
-            apps
+            <div>
+                <h1></h1>
+                <p></p>
+                <div>
+                    <p></p>
+                    <input type="text" />
+                </div>
+            </div>
+            <div className='grid grid-cols-4 gap-4'> 
+                {
+                    
+                }
+            </div>
         </div>
     );
 };
