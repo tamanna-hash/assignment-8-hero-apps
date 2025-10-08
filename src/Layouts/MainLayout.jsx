eb6725facbf944fdfa0c472ecf1c345b0c881d8f
tamanna-hash <tamannaemails@gmail.com>
2025-10-08 22:09:13 +0600

    dist file

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router';
 const MainLayout = () => {
     return (
         <div className=''>
-            <div className=' flex flex-col min-h-screen'>
+            <div className='flex flex-col min-h-screen'>
                 <Navbar />
                 <div className='flex-1'>
                     <Outlet />
